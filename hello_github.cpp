0a00bf8f26bb01aa89127258795d11ce59eedd38
Anurag Dasgupta <dasguptaanurag8@gmail.com>
2026-04-28 03:12:32 +0530

    first commit: added hello_github.cpp

---
+++ hello_github.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // My first code uploaded to GitHub
+    // Anurag Dasgupta — SRM Kattankulathur
+    // Date: April 2026
+    
+    cout << "Hello GitHub! I am Anurag Dasgupta." << endl;
+    cout << "CSE Student at SRM Kattankulathur." << endl;
+    cout << "My goal: MAANG by 2028." << endl;
+    
+    return 0;
+}
